a1d1293d960b30e7fdcae07e0e3d40e8a4c5fd82
Corey Salzano <corey.salzano@gmail.com>
2022-11-08 13:49:08 -0500

    Applies the WordPress coding standard

--- contact-form-7-fix-asset-loads.php
+++ contact-form-7-fix-asset-loads.php
@@ -1,5 +1,11 @@
 <?php
-defined( 'ABSPATH' ) or exit;
+/**
+ * CF7 Fix Asset Loads
+ *
+ * @package CF7_Fix_Asset_Loads
+ */
+
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Plugin Name: Contact Form 7 - Fix Asset Loads
@@ -9,48 +15,44 @@ defined( 'ABSPATH' ) or exit;
  * Author URI: https://github.com/csalzano
  */
 
-class CF7_Fix_Asset_Loads
-{	
+/**
+ * CF7_Fix_Asset_Loads
+ */
+class CF7_Fix_Asset_Loads {
+
 	/**
-	 * cf7_form_was_loaded
-	 * 
 	 * Track whether a form was loaded
 	 *
 	 * @var bool
 	 */
-	var $cf7_form_was_loaded = false;
-	
+	protected $cf7_form_was_loaded = false;
+
 	/**
-	 * add_hooks
+	 * Adds hooks that power the plugin.
 	 *
 	 * @return void
 	 */
-	public function add_hooks()
-	{
-		//Change the Contact Form 7 styles to load in the footer
+	public function add_hooks() {
+		// Change the Contact Form 7 styles to load in the footer.
 		add_action( 'wp_enqueue_scripts', array( $this, 'move_style_loads_to_footer' ), 11 );
 
-		//Listen for a form load
+		// Listen for a form load.
 		add_filter( 'wpcf7_form_action_url', array( $this, 'track_form_load' ) );
 	}
-	
+
 	/**
-	 * maybe_enqueue
-	 * 
 	 * This callback method on the `wp_footer` hook checks our member variable
-	 * to see if a form has been loaded. If not, the script is dequeued. If a 
+	 * to see if a form has been loaded. If not, the script is dequeued. If a
 	 * form has been loaded, the styles are are enqueued.
-	 * 
+	 *
 	 * Contact Form 7 scripts already load in the footer.
 	 *
 	 * @return void
 	 */
-	public function maybe_enqueue()
-	{
-		//Was a form loaded? 
-		if( ! $this->cf7_form_was_loaded )
-		{
-			//No, do not enqueue styles and dequeue the scripts
+	public function maybe_enqueue() {
+		// Was a form loaded?
+		if ( ! $this->cf7_form_was_loaded ) {
+			// No, do not enqueue styles and dequeue the scripts.
 			wp_dequeue_script( 'contact-form-7' );
 			return;
 		}
@@ -63,39 +65,33 @@ class CF7_Fix_Asset_Loads
 			wp_enqueue_style( 'jquery-ui-smoothness' );
 		}
 	}
-	
+
 	/**
-	 * move_style_loads_to_footer
-	 * 
 	 * This callback method on the `wp_enqueue_scripts` hook dequeues all
 	 * stylesheets and adds an action hook so they can be requeued later in the
 	 * footer.
-	 * 
+	 *
 	 * Contact Form 7 scripts already load in the footer, so only the styles
 	 * need to be moved.
 	 *
 	 * @return void
 	 */
-	public function move_style_loads_to_footer()
-	{
+	public function move_style_loads_to_footer() {
 		wp_dequeue_style( 'contact-form-7' );
 		wp_dequeue_style( 'contact-form-7-rtl' );
 		wp_dequeue_style( 'jquery-ui-smoothness' );
 		add_action( 'wp_footer', array( $this, 'maybe_enqueue' ) );
 	}
-	
+
 	/**
-	 * track_form_load
-	 * 
 	 * This callback method on the `wpcf7_form_action_url` does not edit the
 	 * form action URL. It sets our member variable to indicate a form has been
 	 * loaded and returns the URL unchanged.
 	 *
-	 * @param  string $url
+	 * @param  string $url Form action URL.
 	 * @return string
 	 */
-	public function track_form_load( $url )
-	{
+	public function track_form_load( $url ) {
 		$this->cf7_form_was_loaded = true;
 		return $url;
 	}
